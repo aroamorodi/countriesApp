c13e034313477224b56d2eb9acfb4bc6a8b7a4e2
aroamorodi <aroa.moro-diaz@capgemini.com>
2024-05-23 12:27:10 +0200

    Añadido spinner y cambio para que busque cuando el usuario deja de escribir

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,18 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>();
+
   @Input()
   public placeholder: string = '';
 
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(300)
+      )
+      .subscribe(value => {
+      this.onDebounce.emit(value);
+    })
+  }
+
   search(texto: string): void {
     this.onValue.emit(texto);
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
   }
 }
