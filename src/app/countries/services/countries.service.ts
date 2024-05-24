e93f8844ea0bb20b109c96dcfe39804721726e97
aroamorodi <aroa.moro-diaz@capgemini.com>
2024-05-24 09:24:30 +0200

    Guardar en localStorage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -16,7 +16,17 @@ export class CountriesService {
   }
 
   constructor( private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
 
+  private saveToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage() {
+    if (!localStorage.getItem('cacheStore')) return;
+
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
   }
 
   private getCountriesRequest (url: string): Observable<Country[]> {
@@ -39,7 +49,8 @@ export class CountriesService {
     const url = `${this.apiURL}/capital/${term}`
     return this.getCountriesRequest(url)
       .pipe(
-        tap(countries => this.cacheStore.byCapital = { term, countries})
+        tap(countries => this.cacheStore.byCapital = { term, countries}),
+        tap(()=> this.saveToLocalStorage())
       )
   }
 
@@ -47,7 +58,8 @@ export class CountriesService {
     const url = `${this.apiURL}/name/${term}`
     return this.getCountriesRequest(url)
     .pipe(
-      tap(countries => this.cacheStore.byCountries = { term, countries})
+      tap(countries => this.cacheStore.byCountries = { term, countries}),
+      tap(()=> this.saveToLocalStorage())
       );
   }
 
@@ -55,7 +67,8 @@ export class CountriesService {
     const url = `${this.apiURL}/region/${term}`
     return this.getCountriesRequest(url)
     .pipe(
-      tap(countries => this.cacheStore.byRegion = { region: term, countries})
+      tap(countries => this.cacheStore.byRegion = { region: term, countries}),
+      tap(()=> this.saveToLocalStorage())
       );
   }
 }
